de5fe5eef6d7f3b8bc993e3ca2be134956a5dabf
Rushabh Patil <you@example.com>
2022-07-01 20:02:38 +0530

    updating forecast data

--- src/utiils/forecast.js
+++ src/utiils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree temperature . It feels like ' + body.current.feelslike + ' degree temperature')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree temperature . It feels like ' + body.current.feelslike + ' degree temperature. Humidity is ' + body.current.humidity)
         }
     })
 }
